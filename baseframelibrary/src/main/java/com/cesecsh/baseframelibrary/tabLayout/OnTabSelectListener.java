bf7f209b8c792618ad9b7b25d111b53fbac501bf
RockQ <1058737674@qq.com>
2017-03-10 16:53:53 +0800

    添加tablayout可以设置是否是indicator是否按照title的宽度还是text的宽度或者比text稍微长一点

---
+++ baseframelibrary/src/main/java/com/cesecsh/baseframelibrary/tabLayout/OnTabSelectListener.java
@@ -0,0 +1,11 @@
+package com.cesecsh.baseframelibrary.tabLayout;
+
+/**
+ * Created by RockQ on 2017/3/10
+ */
+
+interface OnTabSelectListener {
+    void onTabSelect(int position);
+
+    void onTabReselect(int position);
+}
